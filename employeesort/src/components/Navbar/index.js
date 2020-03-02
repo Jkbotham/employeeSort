21d7d42b6102acf8e5e8f8171130ae55b5afb7ca
James <jamesbotham@gmail.com>
2020-03-02 08:41:24 -0600

    Added break under nav bar

--- employeesort/src/components/Navbar/index.js
+++ employeesort/src/components/Navbar/index.js
@@ -4,9 +4,13 @@ import "./style.css";
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <a className="navbar-brand" href="/">Employee Sort</a>
-        </nav>
+        <div>
+            <nav className="navbar">
+                <a className="navbar-brand" href="/">Employee Sort</a>
+            </nav>
+            <br />
+        </div>
+
     );
 }
 
